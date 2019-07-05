58cf27fc748cc788a56a96de2e9fb50dd502ec01
Kaylyn Kwak <kkwak0803@gmail.com>
2019-07-04 22:33:18 -0700

    complete project_new_path and save functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+    def new
+        @project = Project.new # Idon't think this is needed here
+    end
+
+    def create
+        project_params = params.require(:project).permit(:title, :description, :due_date)
+        @project = Project.new project_params
+        if @project.save
+            flash[:notice] = "A new project is saved!"
+            send plain: "saved!"
+            # redirect_to project_path(@project.id)
+        else
+            # flash[:notice] = "error"
+            render :new
+        end
+    end
+
+    def show
+    end
 end
