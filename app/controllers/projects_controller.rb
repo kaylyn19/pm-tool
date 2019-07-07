f7d80b23d984a341a2e70e226242b4617e584288
Kaylyn Kwak <kkwak0803@gmail.com>
2019-07-07 16:48:56 -0700

    complete create,delete,edit and update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,15 +7,38 @@ class ProjectsController < ApplicationController
         project_params = params.require(:project).permit(:title, :description, :due_date)
         @project = Project.new project_params
         if @project.save
-            flash[:notice] = "A new project is saved!"
-            send plain: "saved!"
-            # redirect_to project_path(@project.id)
+            redirect_to project_path(@project.id), notice: "Saved!"
         else
-            # flash[:notice] = "error"
+            flash[:notice] = "error"
             render :new
         end
     end
 
     def show
+        @project = Project.find params[:id]
+    end
+
+    def index
+        @project = Project.all.order(created_at: :desc)
+    end
+
+    def destroy
+        @project = Project.find params[:id]
+        @project.destroy
+        redirect_to projects_path
+    end
+
+    def edit
+        @project = Project.find params[:id]
+    end
+
+    def update
+        project_params = params.require(:project).permit(:title, :description, :due_date)
+        @project = Project.find params[:id]
+        if @project.update project_params
+            redirect_to project_path(@project), notice: 'updated!'
+        else
+            render :edit
+        end
     end
 end
